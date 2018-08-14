73076b628d3b88af32388065ee803ffab4cfc265
stinodes <stijn.tytgat@ixor.be>
2018-08-14 10:47:08 +0200

    Added gestures to user detail modal!!!1

--- src/App/Dashboard/UserDetailModal.js
+++ src/App/Dashboard/UserDetailModal.js
@@ -1,7 +1,9 @@
 // @flow
 import * as React from 'react';
-import { Animated } from 'react-native';
+import { Animated, Dimensions } from 'react-native';
 import { Coordinator, Element, View } from 'nativesystem';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+
 import { Background, Modal } from '../../Components';
 
 type Props = {
@@ -10,13 +12,42 @@ type Props = {
   startPosition: { x: number, y: number, w: number, h: number },
   onRequestClose: () => any,
 };
-type State = {
+type ComponentState = {
   animation: Animated.Value,
 };
 
-class UserDetailModal extends React.Component<Props, State> {
+class UserDetailModal extends React.Component<Props, ComponentState> {
   state = {
-    animation: new Animated.Value(0),
+    animation: new Animated.Value(Dimensions.get('window').height),
+  };
+
+  height = Dimensions.get('window').height;
+  gestureEvent = Animated.event(
+    [{ nativeEvent: { translationY: this.state.animation } }],
+    { userNativeDriver: true },
+  );
+
+  static getDerivedStateFromProps() {}
+
+  gestureEventStateChange = ({
+    nativeEvent: { state, translationY, velocityY },
+  }: {
+    nativeEvent: { state: number, translationY: number, velocityY: number },
+  }) => {
+    if (state === State.END) {
+      if (translationY < 75)
+        Animated.spring(this.state.animation, {
+          toValue: 0,
+          useNativeDriver: true,
+        }).start();
+      else
+        Animated.spring(this.state.animation, {
+          toValue: this.height,
+          velocity: velocityY,
+          overshootClamping: true,
+          useNativeDriver: true,
+        }).start(this.props.onRequestClose);
+    }
   };
 
   createAnimation = ({
@@ -28,12 +59,26 @@ class UserDetailModal extends React.Component<Props, State> {
   }) => {
     if (toValue === 1)
       return Animated.parallel([
-        Animated.timing(animation, { toValue, duration: 0 }),
-        Animated.spring(this.state.animation, { toValue }),
+        Animated.timing(animation, {
+          toValue,
+          duration: 0,
+          useNativeDriver: true,
+        }),
+        Animated.spring(this.state.animation, {
+          toValue,
+          useNativeDriver: true,
+        }),
       ]);
     return Animated.sequence([
-      Animated.spring(this.state.animation, { toValue }),
-      Animated.timing(animation, { toValue, duration: 0 }),
+      Animated.spring(this.state.animation, {
+        toValue: this.height,
+        useNativeDriver: true,
+      }),
+      Animated.timing(animation, {
+        toValue,
+        duration: 0,
+        useNativeDriver: true,
+      }),
     ]);
   };
 
@@ -43,27 +88,41 @@ class UserDetailModal extends React.Component<Props, State> {
       startPosition: { x, y, w, h },
       sharedNode,
     } = this.props;
+    const height = this.height;
     return (
       <Modal
         visible={visible}
         onRequestClose={this.props.onRequestClose}
         createAnimation={this.createAnimation}>
-        <Coordinator f={1} animation={this.state.animation} inputRange={[0, 1]}>
-          <Element
-            positioning={{ top: 0, bottom: 0, left: 0, right: 0 }}
-            start={{ opacity: 0 }}
-            end={{ opacity: 1 }}>
-            <Background f={1} color="white" />
-          </Element>
-          <Element
-            positioning={{ top: 0, left: 0 }}
-            start={{ x, y }}
-            end={{ x: 0, y: 80 }}>
-            <View w={w} h={h}>
-              {sharedNode}
-            </View>
-          </Element>
-        </Coordinator>
+        <View f={1}>
+          <Coordinator
+            f={1}
+            animation={this.state.animation.interpolate({
+              inputRange: [0, height],
+              outputRange: [height, 0],
+              extrapolate: 'clamp',
+            })}
+            inputRange={[0, height]}>
+            <Element
+              positioning={{ top: 0, bottom: 0, left: 0, right: 0 }}
+              start={{ opacity: 0 }}
+              end={{ opacity: 1 }}>
+              <Background f={1} color="white" />
+            </Element>
+            <Element
+              positioning={{ top: 0, left: 0 }}
+              start={{ x, y }}
+              end={{ x: 0, y: 80 }}>
+              <PanGestureHandler
+                onHandlerStateChange={this.gestureEventStateChange}
+                onGestureEvent={this.gestureEvent}>
+                <View w={w} h={h}>
+                  {sharedNode}
+                </View>
+              </PanGestureHandler>
+            </Element>
+          </Coordinator>
+        </View>
       </Modal>
     );
   }
